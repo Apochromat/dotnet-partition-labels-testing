60ae65d07cdfb7ae7c1b325332ac7570cd78cce1
Stepan Ustimenko <50939552+Apochromat@users.noreply.github.com>
2023-04-27 10:36:42 +0700

    feat(ui): добавил WebUI приложения

---
+++ PartitionLabels.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PartitionLabels.WebUI.Models;
+
+namespace PartitionLabels.WebUI.Controllers;
+
+public class HomeController : Controller {
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger) {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult Index(MainModel model) {
+        string? result;
+        var newModel = new MainModel() {
+            input = model.input
+        };
+        
+        try {
+            result = string.Join(", ", PartitionLabels.Solution(model.input));
+            newModel.result = result;
+        }
+        catch (ArgumentException e) {
+            newModel.result = e.Message;
+        }
+        catch (Exception e) {
+            newModel.result = "";
+        }
+        
+        return View(newModel);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error() {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ PartitionLabels.WebUI/Models/MainModel.cs
@@ -0,0 +1,6 @@
+﻿namespace PartitionLabels.WebUI.Models; 
+
+public class MainModel {
+    public String input { get; set; }
+    public String result { get; set; }
+}
